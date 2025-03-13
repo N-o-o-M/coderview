8d26f8b5adc8fb8637ec43a20e07345f83fae47b
Mushaib <mushaibmo@gmail.com>
2025-03-13 22:33:15 +0530

    clerk convex and stream setup done

---
+++ convex/http.ts
@@ -0,0 +1,57 @@
+import { httpRouter } from "convex/server";
+import { httpAction } from "./_generated/server";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { Webhook } from "svix";
+import { api } from "./_generated/api";
+const http = httpRouter();
+
+http.route({
+  path: "/clerk-webhook",
+  method: "POST",
+  handler: httpAction(async (ctx, request) => {
+    const webhook = process.env.CLERK_WEBHOOK_SECRET;
+    if (!webhook) {
+      throw new Error("CLERK_WEBHOOK_SECRET is not set");
+    }
+    const svix_id = request.headers.get("svix-id");
+    const svix_signature = request.headers.get("svix-signature");
+    const svix_timestamp = request.headers.get("svix-timestamp");
+
+    if (!svix_id || !svix_signature || !svix_timestamp) {
+      return new Response("Missing headers", { status: 400 });
+    }
+    const payload = await request.json();
+    const body = JSON.stringify(payload);
+
+    const wh = new Webhook(webhook);
+    let event: WebhookEvent;
+    try {
+      event = wh.verify(body, {
+        svix_id,
+        svix_signature,
+        svix_timestamp,
+      }) as WebhookEvent;
+    } catch (e) {
+      console.error(e);
+      return new Response("Invalid signature", { status: 400 });
+    }
+    const eventType = event.type;
+    if (eventType === "user.created") {
+      const { id, email_addresses, first_name, last_name, image_url } =
+        event.data;
+      const email = email_addresses[0].email_address;
+      const name = `${first_name || ""} ${last_name || ""}`.trim();
+
+      try {
+        await ctx.runMutation(api.users.syncUser, {
+          clerkId: id,
+          email,
+          name,
+          image: image_url,
+        });
+      } catch (error) {}
+    }
+
+    return new Response("OK", { status: 200 });
+  }),
+});
