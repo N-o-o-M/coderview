c9b07495c38395cdf7750398d578e81f70326509
Mushaib <mushaibmo@gmail.com>
2025-03-15 23:31:14 +0530

    few components added and restructured the files

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,9 +11,9 @@ function Navbar() {
         {/* Logo */}
         <Link
           href="/"
-          className="flex items-center gap-2 font-semibold text-2xl mr-6 font-mono hover:opacity-80 transition-opacity"
+          className="flex items-center gap-2 font-semibold text-4xl mr-6 font-mono hover:opacity-80 transition-opacity"
         >
-          <span className="bg-gradient-to-r from-blue-300 to-blue-500 bg-clip-text text-transparent  hover:from-amber-300 hover:to-amber-500">
+          <span className="bg-gradient-to-r from-blue-300 to-blue-500 bg-clip-text text-transparent  hover:from-amber-100 hover:to-amber-500">
             CoderView
           </span>
         </Link>
